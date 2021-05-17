db219c819c6f9d9e93529d60123af45f2061d662
Jessica Moreira <60614200+Jessica-Moreira@users.noreply.github.com>
2021-05-17 00:40:29 -0700

    Add files via upload
    Got a simple query working in query_SSN.php which is the first link in the main site.

--- homepage/query_ProfessorCNum.php
+++ homepage/query_ProfessorCNum.php
@@ -4,21 +4,33 @@
 </head>
 <body>
 <h1>Professor Database</h1>
-<h2>Titles:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Classrooms:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Meeting Days:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Time:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<script language="PHP">
-echo "\n<b>More PHP Output</b><br />\n";
-</script>
-<form action="sample.php" method="POST">
-Enter the social security number: <input type="text" name="sno" />
+<form action="query_ProfessorCNum.php" method="POST">
+Enter the section number: <input type="text" name="Sec_Num" />
 <input type="submit" />
 </form>
+<!-- <form action="query_ProfessorCNum.php" method="POST">
+Enter course number: <input type="text" name="Course_Num" />
+<input type="submit" />
+</form> -->
+
 <li><a href="http://ecs.fullerton.edu/~cs332c21/index.html" class="button">Back to home!</a></li>
+
+<?php
+$link = mysqli_connect('mariadb', 'cs332c21', 'To2ozohz','cs332c21');
+if(!$link){
+    die('Could not connect:' .mysql_error());
+}
+echo 'Connected successfully<p>';
+
+$query = "SELECT * FROM Enrollments WHERE Sec_Num=" .$_POST["Sec_Num"];
+$result = $link->query($query);
+$row=$result->fetch_assoc();
+printf("Section Num: %s<br>\n", $row["Sec_Num"]);
+printf("Grade: %s<br>\n", $row["Grade"]);
+
+$result->free_result();
+$link->close();
+?>
+
 </body>
 </html>
--- homepage/query_SSN.php
+++ homepage/query_SSN.php
@@ -4,21 +4,33 @@
 </head>
 <body>
 <h1>Professor Database</h1>
-<h2>Titles:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Classrooms:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Meeting Days:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<h2>Time:</h2>
-<?php echo "<p>More PHP Output</p>"; ?>
-<script language="PHP">
-echo "\n<b>More PHP Output</b><br />\n";
-</script>
-<form action="sample.php" method="POST">
-Enter the social security number: <input type="text" name="sno" />
-<input type="submit" />
+
+<form action="query_SSN.php" method="POST">
+Enter Your SSN: <input type="text" name="SSN"/>
+<input type="submit"/>
 </form>
+
 <li><a href="http://ecs.fullerton.edu/~cs332c21/index.html" class="button">Back to home!</a></li>
+
+<?php
+$link = mysqli_connect('mariadb', 'cs332c21', 'To2ozohz','cs332c21');
+if(!$link){
+    die('Could not connect:' .mysql_error());
+}
+echo 'Connected successfully<p>';
+
+$query = "SELECT * FROM Professors WHERE SSN=" .$_POST["SSN"];
+$result = $link->query($query);
+$row=$result->fetch_assoc();
+echo "Name: ",$row["PName"], "<br>";
+// printf("Name: %s<br>\n", $row["PName"]);
+// printf("Title: %s<br>\n", $row["Title"]);
+//printf("Classrooms:%s<br>\n", $row["Last_Name"]);
+//printf("Meeting Days:%s<br>\n", $row["First_Name"]);
+//printf("Times:%s<br>\n", $row["Begin_Time"], $row["End_Time"]);
+$result->free_result();
+$link->close();
+?>
+
 </body>
 </html>
