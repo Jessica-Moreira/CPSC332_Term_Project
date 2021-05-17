c5806d32482c4042be010e605b482a4364d96e30
Jessica Moreira <60614200+Jessica-Moreira@users.noreply.github.com>
2021-05-16 17:02:39 -0700

    Add files via upload
    Updated to use with all four queries.

---
+++ query_SSN.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Professor Database</title>
+</head>
+<body>
+<h1>Professor Database</h1>
+<h2>Titles:</h2>
+<?php echo "<p>More PHP Output</p>"; ?>
+<h2>Classrooms:</h2>
+<?php echo "<p>More PHP Output</p>"; ?>
+<h2>Meeting Days:</h2>
+<?php echo "<p>More PHP Output</p>"; ?>
+<h2>Time:</h2>
+<?php echo "<p>More PHP Output</p>"; ?>
+<script language="PHP">
+echo "\n<b>More PHP Output</b><br />\n";
+</script>
+<form action="sample.php" method="POST">
+Enter the social security number: <input type="text" name="sno" />
+<input type="submit" />
+</form>
+<li><a href="http://ecs.fullerton.edu/~cs332c21/index.html" class="button">Back to home!</a></li>
+</body>
+</html>
